858950aacf2523efa212c95ce9e987d3dba8ddf9
Thomas Pourchel <tpourchel@auchan.fr>
2023-06-19 11:15:29 +0200

    modifs

---
+++ db/data.js
@@ -0,0 +1,28 @@
+const data = [
+  {
+    "nom": "Doe",
+    "prenom": "John",
+    "numero": "67",
+    "equipe": "epsi" 
+  },
+  {
+    "nom": "Toto",
+    "prenom": "Titi",
+    "numero": "14",
+    "equipe": "st-jo" 
+  },
+  {
+    "nom": "Jackson",
+    "prenom": "Michael",
+    "numero": "26",
+    "equipe": "epsi" 
+  },
+  {
+    "nom": "Pourchel",
+    "prenom": "Thomas",
+    "numero": "69",
+    "equipe": "st-jo" 
+  }
+]
+
+return module.exports = data
